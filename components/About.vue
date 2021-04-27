ca973392f6da5eb275e61cea664c113ea7627b25
Tristan <tristanmayofreire@gmail.com>
2021-04-27 21:08:42 +0200

    changed a bit of the about me text

--- components/About.vue
+++ components/About.vue
@@ -6,13 +6,49 @@
     </div>
     <div class="right vertical-align">
       <h2>Sobre Mí</h2>
-      <p>Mi primer encuentro con el mundo del desarrollo web fue en Bachillerato. Tuve un profesor magnífico con quien aprendí las bases de HTML, CSS, Javascript y Visual Basic. Más tarde, comenzé mi carrera profesional estudiando LEINN (Grado en Liderazgo Emprendimiento e Innovación) de la Universidad de Mondragon en Madrid. Una formación similar a ADE (Administración y Dirección de Empresas) pero con un enfoque más práctico. Durante el primer año trabaje con Wordpress (sin tener aún conocimientos de programación) y desde entonces he pasado mucho tiempo mejorando mis habilidades.</p>
+      <p>
+        Mi primer encuentro con el mundo del desarrollo web fue en Bachillerato.
+        Tuve un profesor magnífico con quien aprendí las bases de HTML, CSS,
+        Javascript y Visual Basic. Más tarde, comenzé mi carrera profesional
+        estudiando LEINN (Grado en Liderazgo Emprendimiento e Innovación) de la
+        Universidad de Mondragon en Madrid. Una formación similar a ADE
+        (Administración y Dirección de Empresas) pero con un enfoque más
+        práctico. Durante el primer año trabaje con Wordpress (sin tener aún
+        conocimientos de programación) y desde entonces he pasado mucho tiempo
+        mejorando mis habilidades.
+      </p>
       <br />
-      <p>Más tarde, entré a formar parte de Demium Startups, una incubadora pre-idea y pre-equipo, donde trabajé y aprendí muchisimo y conocí a profesionales increíbles. Descubrí la importancia de ser tenaz. Continuamente me enfrentaba a problemas que no sabía solucionar. Hubiera sido muy fácil tirar la toalla pero es en esos momentos, en los que no ves salida alguna, cuando más aprendes y creces. Además, tuve que adaptarme a un entorno de alta velocidad, pues la rapidez es una de las ventajas clave que tiene una startup frente a sus competidores más establecidos.</p>
+      <p>
+        Más tarde, entré a formar parte de Demium Startups, una incubadora
+        pre-idea y pre-equipo, donde trabajé y aprendí muchisimo y conocí a
+        profesionales increíbles. Descubrí la importancia de ser tenaz.
+        Continuamente me enfrentaba a problemas que no sabía solucionar. Hubiera
+        sido muy fácil tirar la toalla pero es en esos momentos, en los que no
+        ves salida alguna, cuando más aprendes y creces. Además, tuve que
+        adaptarme a un entorno de alta velocidad, pues la rapidez es una de las
+        ventajas clave que tiene una startup frente a sus competidores más
+        establecidos.
+      </p>
       <br />
-      <p>Decidí dar el paso y probar un mundo por el que me sentía atraido desde hacia años, el desarrollo web. Tenía la intención de matricularme en Ingenieria Informática, pero faltaban meses para la temporada y decidí comenzar por mi cuenta. Empecé haciendo varios cursos online de HTML, CSS y Javascript, así como una formación de introducción a la programación ofrecida por Harvard.</p>
+      <p>
+        Decidí dar el paso y probar un mundo por el que me sentía atraido desde
+        hacia años, el desarrollo web. Tenía la intención de matricularme en
+        Ingenieria Informática, pero faltaban meses para la temporada y decidí
+        comenzar por mi cuenta. Empecé haciendo varios cursos online de HTML,
+        CSS y Javascript, así como una formación de introducción a la
+        programación ofrecida por Harvard.
+      </p>
       <br />
-      <p>Siempre he disfrutado mucho de aprender por mi cuenta. Es un reto personal de dedicación y distiplina tan grande como gratificante. Pero nunca me había puesto un objetivo de ese calibre y mentiría si dijera que ha sido fácil. Sin embargo, para cuando llego la temporada de matriculas, había aprendido suficiente como para sentir que podría seguir haciéndolo sin necesidad de un Bootcamp o grado universitario. Ahora busco oportunidades para seguir desarrollandome en este ámbito.</p>
+      <p>
+        Siempre he disfrutado mucho de aprender por mi cuenta. Es un reto
+        personal de dedicación y distiplina tan grande como gratificante. Pero
+        nunca me había puesto un objetivo de ese calibre y mentiría si dijera
+        que ha sido fácil. Sin embargo, no me arrepiento en lo más mínimo, cada
+        nueva tecnología que aprendo, cada proyecto que realizo me refirma en
+        que estoy donde debería estar, que voy por buen camino. Por eso
+        continuamente busco oportunidades para seguir desarrollandome en este
+        ámbito.
+      </p>
     </div>
   </div>
 </template>
